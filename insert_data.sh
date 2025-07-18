74f4b88e409701a5c8464240c6d80ad9eaf661d9
Lavie <bellotaophyc@outlook.com>
2025-07-18 09:01:57 +0200

    delete the data from majors, students, and majors_courses table because the majors table use some data there as a foreign key

--- insert_data.sh
+++ insert_data.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 # Script to insert data from courses.csv and students.csv into students database
 
-cat courses_test.csv | while IFS="," read MAJOR COURSE
 PSQL="psql -X --username=lavie --dbname=students --no-align --tuples-only -c"
+
+cat courses_test.csv | while IFS="," read -r MAJOR COURSE
 do
+    # Skip header row
+    if [[ "$MAJOR" == "major" ]]; then
+        continue
+    fi
+
     # get major_id
     MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
-    echo $MAJOR_ID
+    # echo "Major ID: $MAJOR_ID"
+    
     # if not found
-    if [[ -z $MAJOR_ID ]]; then
+    if [[ -z "$MAJOR_ID" ]]
+    then
         # insert major
-        INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR')")
-        echo $INSERT_MAJOR_RESULT
-        # get new major_id
-        # MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+        INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR') RETURNING major_id")
+        echo "Inserted major: $INSERT_MAJOR_RESULT"
+        MAJOR_ID=$INSERT_MAJOR_RESULT
     fi
-    # insert major
 
     # get new major_id
+
+    # if not found
+
+    # insert course
+
+    # get new course_id
 
     # if not found
 
