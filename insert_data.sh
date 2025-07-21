380bfd5cbc9e944a149a4a5141173836add498d2
Lavie <bellotaophyc@outlook.com>
2025-07-21 11:03:58 +0200

    adding a TRUNCATE command rather than manually deleting the data

--- insert_data.sh
+++ insert_data.sh
@@ -3,6 +3,8 @@
 
 PSQL="psql -X --username=lavie --dbname=students --no-align --tuples-only -c"
 
+echo $($PSQL "TRUNCATE students, majors_courses, courses, majors")
+
 cat courses_test.csv | while IFS="," read MAJOR COURSE
 
 do
@@ -18,14 +20,35 @@ do
         # insert major
         INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR')")
         
+        if [[ $INSERT_MAJOR_RESULT == "INSERT 0 1" ]]
+        then
+            echo "Inserted into majors, $MAJOR"
+        fi
+
         # get new major_id
         MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
         echo "New MAJOR_ID: $MAJOR_ID"
     fi
 
     # get course_id
+    COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")
 
     # if not found
+    if [[ -z $COURSE_ID ]]
+    then
+        # insert course
+        INSERT_COURSE_RESULT=$($PSQL "INSERT INTO courses(course) VALUES('$COURSE')")
+        
+        if [[ $INSERT_COURSE_RESULT == "INSERT 0 1" ]]
+        then
+            echo "Inserted into courses, $COURSE"
+        fi
+
+        # get new course_id
+        # COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")
+        # echo "New COURSE_ID: $COURSE_ID"
+    fi
+
 
     # insert course
 
